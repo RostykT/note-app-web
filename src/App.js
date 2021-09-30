954f67da80b9e969b862a582decc9525de8b15b6
Rostyk <rostishka1@gmail.com>
2021-10-01 00:58:54 +0300

    dark mode, using localStorage even after reload page, we store state and render it

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import NoteList from "./components/NoteList";
 import { nanoid } from "nanoid";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Search from "./components/Search";
+import Header from "./components/Header";
 
 function App() {
 	const [notes, setNotes] = useState([
@@ -40,20 +41,38 @@ function App() {
 
 	const [SearchText, setSearchText] = useState("");
 
+	const [darkMode, setDarkMode] = useState(false);
+
+	useEffect(() => {
+		const savedNotes = JSON.parse(
+			localStorage.getItem("react-notes-app-data")
+		);
+		if (savedNotes) {
+			setNotes(savedNotes);
+		}
+	}, []);
+
+	useEffect(() => {
+		localStorage.setItem("react-notes-app-data", JSON.stringify(notes));
+	}, [notes]);
+
 	const deleteNote = (id) => {
 		const newNotes = notes.filter((note) => note.id !== id);
 		setNotes(newNotes);
 	};
 	return (
-		<div className="container">
-			<Search handleSearchNote={setSearchText} />
-			<NoteList
-				notes={notes.filter((note) =>
-					note.text.toLowerCase().includes(SearchText)
-				)}
-				handleAddNote={addNote}
-				handleDeleteNote={deleteNote}
-			/>
+		<div className={`${darkMode && "dark-mode"}`}>
+			<div className="container">
+				<Header handleToggleDarkMode={setDarkMode} />
+				<Search handleSearchNote={setSearchText} />
+				<NoteList
+					notes={notes.filter((note) =>
+						note.text.toLowerCase().includes(SearchText)
+					)}
+					handleAddNote={addNote}
+					handleDeleteNote={deleteNote}
+				/>
+			</div>
 		</div>
 	);
 }
